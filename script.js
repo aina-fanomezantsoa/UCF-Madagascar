b03352e08366cc2c6f03cc7cfbee7f19c67a0642
aina-fanomezantsoa <ainafanomezantsoa37@gmail.com>
2025-09-12 16:47:33 +0300

    finalisation

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const navPopup = document.getElementById("pop_up")
-const btnMenu = document.getElementById("btn_menu")
-const recherche = document.getElementById("reach")
-const nav = document.getElementById("navigation")
-const head = document.getElementById("head")
-const navAcceuil=document.getElementById("navAcceuil")
+const navPopup = document.getElementById("pop_up");
+const btnMenu = document.getElementById("btn_menu");
+const send = document.getElementById("btnSend");
+const objet =document.getElementById("objet");
+const message = document.getElementById("message");
+let email = "ainafanomezantsoa37@gmail.com"
 
 btnMenu.addEventListener("click", ()=>{
     navPopup.classList.toggle("menu_clicked");
     btnMenu.classList.toggle("focus");
 })
+send.addEventListener("click", ()=>{
+    if (objet.value === "" | message.value === ""){
+        return;
+    }else{
+        alert("Message envoyé!! Objet = "+ objet.value)
+
+        objet.value="";
+        message.value="";
+    }
+    
+})
+function envoyerEmail(){
+    window.onload = function(){
+        send.addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+    }
+}
