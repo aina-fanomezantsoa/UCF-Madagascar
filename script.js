247aa065dcebe5f7a260182b0354937e95fb637d
aina-fanomezantsoa <ainafanomezantsoa37@gmail.com>
2025-09-19 09:55:02 +0300

    post recherche

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const btnMenu = document.getElementById("btn_menu");
 const send = document.getElementById("btnSend");
 const objet =document.getElementById("objet");
 const message = document.getElementById("message");
-let email = "ainafanomezantsoa37@gmail.com";
 const formation = document.getElementById("formation");
 const btnFormation = document.getElementById("btnFormation");
 
@@ -15,24 +14,12 @@ send.addEventListener("click", ()=>{
     if (objet.value === "" | message.value === ""){
         return;
     }else{
-        alert("Message envoyé!! Objet = "+ objet.value)
+        const destinataire = "univcfm@gmail.com";
+        const mailtoLink = `mailto:${destinataire}?subject=${encodeURIComponent(objet.value)}&body=${encodeURIComponent(message.value)}`;
+        window.open(mailtoLink, "_blank");
 
         objet.value="";
         message.value="";
     }
     
 })
-function envoyerEmail(){
-    window.onload = function(){
-        send.addEventListener('submit', function(event) {
-                event.preventDefault();
-                // these IDs from the previous steps
-                emailjs.sendForm('contact_service', 'contact_form', this)
-                    .then(() => {
-                        console.log('SUCCESS!');
-                    }, (error) => {
-                        console.log('FAILED...', error);
-                    });
-            });
-    }
-}
